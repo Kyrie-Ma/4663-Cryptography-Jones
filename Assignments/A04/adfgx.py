f8147e99c52b3258e42ccbe5f17a88b3f11d9cb5
Landen Jones <landensjones@yahoo.com>
2020-11-12 09:59:26 -0600

    :)

--- Assignments/A04/adfgx.py
+++ Assignments/A04/adfgx.py
@@ -1,166 +1,280 @@
+# python3 Assignments/A04/adfgx.py input="Assignments/A04/plaintext" output="Assignments/A04/cipherText" op=encrypt key1=machine key2=trex
 import sys
-import pprint as pp
+import math
 import os
-from os import path
-
+from createPolybius import AdfgxLookup
 
 def mykwargs(argv):
+    '''
+    Processes argv list into plain args (list) and kwargs (dict).
+    Just easier than using a library like argparse for small things.
+    '''
     args = []
     kargs = {}
+
     for arg in argv:
         if '=' in arg:
             key,val = arg.split('=')
             kargs[key] = val
         else:
             args.append(arg)
-    return args, kargs
+    return args,kargs
+
+# Given
+def print_matrix(matrix,rows):
+    """ 
+    Print the matrix so we can visually confirm that our
+    columnar transposition is actually working
+    """
+    for k in matrix:
+        print(k, end = '_')
+    print("")
+    for k in matrix:
+        print('-',end=' ')
+
+    print("")
+    for r in range(rows):
+        for k in matrix:
+            if r < len(matrix[k]):
+                print(matrix[k][r],end=" ")
+            else:
+                print(" ",end=' ')
+        print("")
+    
+# Given
+def get_message(text, key1):
+    """
+    Use the createPolybius file to make a Polybius square and get the new message using key1
+    """
+    # init and input my first keyword
+    A = AdfgxLookup(key1)
+    # build my lookup table 
+    lookup = A.build_polybius_lookup()
+    # print out the actual matrix to the screen
+    A.sanity_check()
+    message = ''
+    # create the message using the lookup table
+    for c in text:
+      message = (message + ' ' + lookup[c] + ' ')
+    print("")
+    # get rid of spaces
+    message = message.replace(' ','')
+    print(message)
+    return message
+
+def encrypt():
+    """
+    Read from the plainText file and call get_message and encrypt_message to encrypt the plain text by the ADFGX Cipher
+    """
+    with open(input) as f:
+      text = f.read()
+    
+    text = text.lower()
+    print("Plain text: " + text)
+    text = text.replace(' ','')
+
+    codedMessage = get_message(text, key1)
+    
+    encrypt_message(codedMessage)
+
+def encrypt_message(message):
+    """
+    Receive the message from get_message and fill into a matrix with the indices of the letters of key2. 
+    Then alphabetize key2 to transpose the columns and mix up the matrix. 
+    The encrypted message is the characters of the matrix displayed one by one.
+    """
+    key2_length = len(key2)             # length of key
+    message_length = len(message)       # message length 
+    rows = math.ceil(float(message_length)/float(key2_length))
+
+    # dictionary for the new matrix
+    matrix = {}
+
+    # every letter is a key that points to a list
+    for k in key2:
+          matrix[k] = []
+
+    # add the message to the each list in a row-wise fashion
+    i = 0
+    for m in message:
+        matrix[key2[i]].append(m)
+        i += 1
+        i = i % len(key2)
+
+    print("")
+    print_matrix(matrix,rows)
+
+    # Alphabetize the matrix
+    temp_matrix = sorted(matrix.items())
+    print("")
+
+    sorted_matrix = {}
+    # Rebuild the sorted matrix into a dictionary again
+    for item in temp_matrix:
+        sorted_matrix[item[0]] = item[1]
+
+    # Check what we have
+    print_matrix(sorted_matrix,rows)
+    print("")
+
+    # Print the cypherText
+    print_message(sorted_matrix,key2)
 
 
-class AdfgxLookup:
-    def __init__(self, k=None):
-        # Gets rid of duplicate characters in key1
-        self.key = self.remove_duplicates(k)
-        # Declares all letters in alphabet to check with later
-        self.alphabet = [chr(x+97) for x in range(26)]
-        # 
-        self.adfgx = ['A', 'D', 'F', 'G', 'X']
-        
-        self.keylen = 0     # Initialize length of key
-
-        if self.key:        # Changes key length if we have key
-            self.keylen = len(self.key)
-                            
-        self.polybius = None    # 
-        self.lookup = None      # 
-
-    def remove_duplicates(self, key):
-        """ Removes duplicate letters from a given key, since they
-            will break the encryption.
-
-            Example: 
-                key = 'helloworldhowareyou'
-                returns 'helowrdayu'
-
-        """
-        newkey = []             # create a list for letters
-        for i in key:           # loop through key
-            if not i in newkey:  # skip duplicates
-                newkey.append(i)
-        
-        # create a string by joining the newkey list as a string
-        return ''.join(str(x) for x in newkey)
-
-    def build_polybius_string(self,key=None):
-        """Builds a string consisting of a keyword + the remaining
-           letters of the alphabet. 
-           Example:
-                key = 'superbatzy'
-                polybius = 'superbatzycdfghiklmnoqvwx'
-        """
-        # no key passed in, used one from constructor
-        if key:
-            self.key = self.remove_duplicates(key)
-        else:
-            print("Error: There is NO key defined to assist with building of the matrix")
-            sys.exit(0)
-
-        # key exists ... continue
-        self.keylen = len(self.key)
-
-        # prime polybius_string variable with key
-        self.polybius = self.key
-
-        for l in self.alphabet:
-            if l == 'j':        # no j needed!
-                continue
-            if not l in self.key:    # if letter not in key, add it
-                self.polybius += l
-        return self.polybius
-
-    def build_polybius_lookup(self,key=None):
-        if key != None:
-            self.key = self.remove_duplicates(key)
-
-        # NO key!
-        if not self.key:
-            print("Error: There is NO key defined to assist with building of the matrix")
-            sys.exit(0)
-
-        # no polybius built, make one!
-        if self.polybius == None:
-            self.build_polybius_string()
-
-        # init our dictionary
-        self.lookup = {}            # dict as our adfgx reverse lookup
-        for l in self.polybius:     # loop through the 1D matrix we created
-            self.lookup[l] = ''     # init keys in the dictionary
-
-        row = 0 
-        col = 0
-
-        # loop through the polybius 1D string and get the 2 letter pairs
-        # needed to do the initial encryption
-        for row in range(5):
-            for col in range(5):
-                i = (5 * row) + col
-                self.lookup[self.polybius[i]] = self.adfgx[row]+self.adfgx[col]
-
-        return self.lookup
+# Given
+def print_message(matrix,key2word):
+    """
+    Open the output file and print the message both to the screen and the output file
+    """
+    ofile = open(output, "w")
+    
+    i = 1
+    #print to the screen and to the file
+    for k in sorted(key2word):
+      for d in matrix[k]:
+        print(d,end='') 
+        ofile.write(d)
+        # the spaces between every two letters is only for appearance
+        if i % 2 == 0:
+            print(' ',end='')
+            ofile.write(' ')
+        i += 1
+    print("")
 
 
-    def sanity_check(self):
 
-        if not self.key:
-            print("Error: There is NO key defined to assist with building of the matrix")
-            sys.exit(0)
+def decrypt():
+    """
+    We have both keys and the cypherText.
+    Get the number of long columns so that the matrix can be filled correctly. 
+    Fill message into the alphabetized matrix, then unalphabetize.
+    Read your message from the matrix, and then plug it in to the
+    reversed polybius lookup table with the first key and acquire your decrypted plaintext.
+    """
+    with open(output) as f:
+      cipherText = f.read()
+    
+    cipherText = cipherText.replace(' ','')
 
-        # no polybius built, make one!
-        if self.polybius == None:
-            self.build_polybius_string()
+    infile = open(input, "w")
+    print("")
+    print("Cyphered Text is " + cipherText)
+    print("")
+    key2_length = len(key2)
+    cipherText_length = len(cipherText)
+    rows = math.ceil(float(cipherText_length)/float(key2_length))
 
-        row = 0
-        col = 0
-       
-        sys.stdout.write('\n  ')
-        for l in self.adfgx:
-            sys.stdout.write(l+' ')
-        sys.stdout.write('\n')
-        for l in self.adfgx:
-            sys.stdout.write(l+' ')
-            for l in self.adfgx:
-                i = (5 * row) + col
-                sys.stdout.write(self.polybius[i]+' ')
-                col += 1
-            row += 1
-            col = 0
-            sys.stdout.write("\n")
+    # create empty matrix
+    matrix = {k: [] for k in sorted(list(key2))}
+
+    long_cols = cipherText_length % key2_length
+    # integer division, //, gives the number of chars in a column
+    col_length = cipherText_length // key2_length
+    long_cols_lookup = []
+    short_cols_lookup = []
+    for index, column in enumerate(key2):          
+      if index < long_cols:
+        long_cols_lookup.append(key2[index])
+      else:
+        short_cols_lookup.append(key2[index])
+    
+    tempKey2 = sorted(key2)
+   
+    long_cols_length = col_length + 1
+    
+    i = 0
+    for index, column in enumerate(tempKey2): 
+      if tempKey2[index] in long_cols_lookup:
+        for _ in range(long_cols_length):
+          matrix[column].append(cipherText[i])
+          i += 1
+      if tempKey2[index] in short_cols_lookup:
+        for _ in range(col_length):
+          matrix[column].append(cipherText[i])
+          i += 1
+
+    print_matrix(matrix, rows)
+    print("")
+
+    # Alphabetize the matrix
+    temp_matrix = { k: matrix[k] for k in key2 }
+
+    print_matrix(temp_matrix,rows)
+    print("")
+
+    message = ''
+    for r in range(rows):
+        for k in temp_matrix:
+            if r < len(temp_matrix[k]):
+                message = message + temp_matrix[k][r]
+
+    print("Message is " + message)
+    
+    # Now that you have the message, get the plain text by using each 2 characters as coordinates
+
+    pText = get_plaintext(message, key1)
+    print("Plain text is " + pText)
+    infile.write(pText)
 
 
-if __name__ == '__main__':
-    argv = sys.argv[1:]  # Command Parameter arguments stored
-    args, kwargs = mykwargs(argv)     # Command parameters placed into a dictionary
-    input_file = kwargs.get('input', None)   # Gets inputfile
-    output_file = kwargs.get('output', None)  # Gets outputfile
-    task = kwargs.get('task',None)          # Encrypt or Decrypt
-    key1 = kwargs.get('key1',None)            # Gets First Key
-    key2 = kwargs.get('key2',None)            # Gets First Key
-    if input_file:  # Gets plain text from input file
-        input_file = path.join(path.dirname(__file__), input_file)
-        with open(input_file) as f:
-            plaintext = f.read()
+
+def get_plaintext(text, key1):
+    """
+    Reverse the polybius lookup table and use every 2 letters of the message to find the corresponding plain text character. 
+    Concatenate these to get the decrypted plain text.
+    """
+    B = AdfgxLookup(key1)
+
+    # build my lookup table and reverse it
+    lookup2 = B.build_polybius_lookup()
+    reverse_lookup = {value : key for (key, value) in lookup2.items()}
+    
+    B.sanity_check()
+
+    pText = ''
+    twoLetters = ""
+    i = 1
+    # use every 2 letters of the message in the lookup table
+    for c in text:
+      twoLetters = twoLetters + c
+      mod = i % 2
+      # if we have read two more letters from the message
+      if mod == 0:
+        newLetter = reverse_lookup[twoLetters]
+        pText = (pText + newLetter)
+        twoLetters = ""
+      i = i + 1
+    
+    print("")
+    print(pText)
+    # return the plain text
+    return pText
+    
+
+
+def usage():
+    """
+    Display descriptive error messages if the keywords from .replit are incorrect
+    """
+    name = os.path.basename(__file__)
+    print(f"Usage: python {name} [input=string filename] [output=string filename] [key=string] [op=encrypt/decrypt]")
+    print(f"Example:\n\t python {name} input=in1 output=out1 op=encrypt key1=machine key2=trex \n")
+    sys.exit()
+
+
+if __name__ == "__main__":
+    # get processed command line arguments 
+    _,params = mykwargs(sys.argv[1:])
+    input = params.get("input", None)
+    output = params.get("output", None)
+    operation = params.get("op", None)
+    key1 = params.get("key1", None)
+    key2 = params.get("key2", None)
+    if not input and not output and not operation and not key1 and not key2:
+        usage()
+    if operation == "encrypt":
+        encrypt()
+    elif operation == "decrypt":
+        decrypt()
     else:
-        plaintext = "therewasnoplaintextspecified"
-        
-    if task == "encrypt":
-        A = AdfgxLookup(key1)
-        
-        # build my lookup table 
-        lookup = A.build_polybius_lookup()
-        print(A.polybius)
-        pp.pprint(lookup)
-        ALPHABET = [chr(x+97) for x in range(26)]
-        ciphertext = ""
-        for c in plaintext:
-            if c in ALPHABET:
-                ciphertext += lookup[c]
-        print(ciphertext)
+        usage()
